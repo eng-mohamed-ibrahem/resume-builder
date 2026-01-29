c763b0099f5c8ae7130b63f42445ae15d862b91e
mohamed_ibrahem <mohamed.data13@gmail.com>
2026-01-30 00:35:58 +0200

    Built web
    Branch: main
    Commit: 69e5a88b5fc2c9e60ca9667b64a0887d2b58b221
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "27210781"
+    serviceWorkerVersion: "1435381462"
   }
 });
